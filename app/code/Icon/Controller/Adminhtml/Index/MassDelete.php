605702946963fc07153b474348d2073085c0d119
thuannd6703 <thuannd6703@co-well.com.vn>
2018-07-19 17:37:24 +0700

    add module

---
+++ app/code/Icon/Controller/Adminhtml/Index/MassDelete.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Icon\Controller\Adminhtml\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+use Icon\Model\ResourceModel\Icon\CollectionFactory;
+/**
+ * Description of MassDelete
+ *
+ * @author DuyTN
+ */
+class MassDelete extends \Magento\Backend\App\Action{
+    /**
+     * @var Filter
+     */
+    protected $filter;
+ 
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+ 
+ 
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+    /**
+     * Execute action
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     * @throws \Magento\Framework\Exception\LocalizedException|\Exception
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+        foreach ($collection as $item) {
+            $item->delete();
+        }
+ 
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+ 
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('icon/index/index');
+    }
+
+    protected function _isAllowed()
+    {
+     return $this->_authorization->isAllowed('Icon::delete');
+    }
+}
---
+++ app/code/Icon/Controller/Adminhtml/Index/Save.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Icon\Controller\Adminhtml\Index;
+
+/**
+ * Description of Save
+ *
+ * @author DuyTN
+ */
+class Save extends \Magento\Backend\App\Action{
+    protected $dataPersistor;
+    protected $authSession;
+    protected $date;
+    protected $iconFactory;
+    protected $helper;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\App\Request\DataPersistorInterface $dataPersistor,
+        \Magento\Framework\Stdlib\DateTime\DateTime $date,
+        \Magento\Backend\Model\Auth\Session\Proxy $authSession,
+        \Icon\Model\IconFactory $iconFactory,
+        \Icon\Helper\Data $helper
+    ) {
+        $this->dataPersistor = $dataPersistor;
+        $this->date = $date;
+        $this->authSession = $authSession;
+        $this->iconFactory = $iconFactory;
+        $this->helper = $helper;
+        parent::__construct($context);
+    }
+    
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+
+        $data = $this->getRequest()->getPostValue();
+        if ($data) {
+            $max = $this->helper->getMaxCode();
+            $id = $this->getRequest()->getParam('icon_id');
+            $model = $this->iconFactory->create()->load($id);
+            //Check if already exist new icon
+            if (!$id) {
+                if (!$max || $max < 1000) {
+                    $data['code'] = 1000;
+                } else {
+                    $data['code'] = $max + 1;
+                }
+            }
+            if (!$model->getIconId() && $id) {
+                $this->messageManager->addErrorMessage(__('This Icon no longer exists.'));
+                return $resultRedirect->setPath('*/*/');
+            }
+            $data['upuser_id'] = $this->authSession->getUser()->getId();
+
+            $model->setData($data);
+        
+            try {
+                $model->save();
+                $this->messageManager->addSuccessMessage(__('You saved the Icon.'));
+                $this->dataPersistor->clear('icon_icon');
+        
+                if ($this->getRequest()->getParam('back')) {
+                    return $resultRedirect->setPath('*/*/edit', ['icon_id' => $model->getIconId()]);
+                }
+                return $resultRedirect->setPath('*/*/');
+            } catch (\LocalizedException $e) {
+                $this->messageManager->addErrorMessage($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addExceptionMessage($e, __('Something went wrong while saving the Icon.'));
+            }
+        
+            $this->dataPersistor->set('icon_icon', $data);
+            return $resultRedirect->setPath('*/*/edit', ['icon_id' => $this->getRequest()->getParam('icon_id')]);
+        }
+        return $resultRedirect->setPath('*/*/');
+    }
+    
+    protected function _isAllowed()
+    {
+     return $this->_authorization->isAllowed('Icon::icons');
+    }
+}
