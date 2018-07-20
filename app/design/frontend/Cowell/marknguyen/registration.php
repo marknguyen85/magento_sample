c3d247e8aada675a96e11704a85fe4827e8714c0
thuannd6703 <thuannd6703@co-well.com.vn>
2018-07-20 23:45:48 +0700

    add theme

---
+++ app/design/frontend/Cowell/marknguyen/registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * ExampleThemeSkeleton theme for Magento
+ *
+ * @package     Yireo_ExampleThemeSkeleton
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2018 Yireo (https://www.yireo.com/)
+ * @license     OSL
+ */
+ 
+use Magento\Framework\Component\ComponentRegistrar;
+ 
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Cowell/marknguyen',
+    __DIR__
+);
